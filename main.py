a505472bd6a6a4e97ede3926423d8d687035873c
Arslan <105282949+arslan-aralbaev@users.noreply.github.com>
2023-03-11 16:57:10 +0500

    Programming update #2 - add new function - set point on button 'p' - patch bug

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import pygame as py
 import requests
 import io
@@ -9,10 +8,12 @@ x, y = screen.get_size()
 update_flag = True
 last = ''
 ll = [32.530887, 55.703118]
-scale, mapZoom = 2.0, 14
+scale, mapZoom = 2.0, 15
 landArray = ['map', 'sat', 'skl', 'trf']
+pmArray = list()
 lCount = 0
 past = ''
+kToMoveMap = 0.002
 
 
 def get_image(response):
@@ -21,14 +22,27 @@ def get_image(response):
     return py.image.load(img)
 
 
-def update(name='map.png', longLat=(37.530887, 55.703118), mapScale=1.0, land='map', zoom=14):
-    global update_flag
+def create_base_point(anypm, long, lat):
+    if pmArray:
+        pmArray.append('~' + str(long) + ',' + str(lat))
+    else:
+        pmArray.append('&' + anypm + '=' + str(long) + ',' + str(lat))
+    print(pmArray, pmArray[-1])
+
+
+def update(mode, name='map.png', longLat=(37.530887, 55.703118), mapScale=1.0, land='map', zoom=14):
+    global update_flag, pmArray
     if update_flag:
         screen.fill((255, 0, 255))
-        if land == 'sat':
-            map_request = f'http://static-maps.yandex.ru/1.x/?ll={float(longLat[0])},{float(longLat[1])}&z={zoom}&spn=0.002,0.002&l={land}'
+        if mode == 'test':
+            map_request = f'https://static-maps.yandex.ru/1.x/?l=map&z={zoom}&pt2'
+        elif land == 'saat':
+            map_request = f'http://static-maps.yandex.ru/1.x/?ll={float(longLat[0])},{float(longLat[1])}' \
+                f'&z={zoom}&spn=0.002,0.002&l={land}'
         else:
-            map_request = f'http://static-maps.yandex.ru/1.x/?ll={float(longLat[0])},{float(longLat[1])}&z={zoom}&pn=0.002,0.002&l={land}'
+            map_request = f'http://static-maps.yandex.ru/1.x/?ll={float(longLat[0])},{float(longLat[1])}' \
+                f'&z={zoom}&pn=0.002,0.002&l={land}' + ','.join(pmArray)
+        print(map_request)
         response = requests.get(map_request)
         if not response:
             print("Ошибка выполнения запроса:")
@@ -52,13 +66,31 @@ while True:
     keys = py.key.get_pressed()
     scale += 0.1 if keys[py.K_PAGEUP] and (scale < 3.9) else 0
     scale -= 0.1 if keys[py.K_PAGEDOWN] and (1.1 < scale) else 0
+    if keys[py.K_UP] and ll[1] > 0:
+        ll[1] += mapZoom * kToMoveMap
+        py.time.delay(250)
+    if keys[py.K_DOWN] and ll[1] < 180:
+        ll[1] -= mapZoom * kToMoveMap
+        py.time.delay(250)
+    if keys[py.K_LEFT] and ll[0] > 0:
+        ll[0] -= (0 + mapZoom) * kToMoveMap
+        py.time.delay(250)
+    if keys[py.K_RIGHT] and ll[0] < 360:
+        ll[0] += mapZoom * kToMoveMap
+        py.time.delay(250)
     if keys[py.K_w] and mapZoom < 23:
         mapZoom += 1
         py.time.delay(250)
-    elif keys[py.K_s] and mapZoom > 0:
+    if keys[py.K_s] and mapZoom > 0:
         mapZoom -= 1
         py.time.delay(250)
-    elif keys[py.K_m]:
+    if keys[py.K_q]:
+        exit()
+    if keys[py.K_p] and len(pmArray) < 99:
+        create_base_point('pt', ll[0], ll[1])
+        print('key_down')
+        py.time.delay(250)
+    if keys[py.K_m]:
         lCount = 0 if lCount == 3 else lCount+1
         py.time.delay(250)
-    update('map.png', ll, scale, landArray[lCount], mapZoom)
+    update('work', 'map.png', ll, scale, landArray[lCount], mapZoom)
